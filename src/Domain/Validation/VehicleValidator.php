e915fa3cb5762d9c3ea0186e764cf92b14ab1dd3
Grzegorz Kadaj <grzegorz.kadaj@devmouse.pl>
2025-10-19 18:47:39 +0200

    Small fix

--- src/Domain/Validation/VehicleValidator.php
+++ src/Domain/Validation/VehicleValidator.php
@@ -10,13 +10,13 @@ class VehicleValidator
     public function validate(VehicleDTO $vehicleDTO): bool
     {
         if (empty($vehicleDTO->registrationNumber) || strlen($vehicleDTO->registrationNumber) > 16) {
-            throw new \InvalidArgumentException('Registration number is required and must be less or equal than 16 characters');
+            throw new \InvalidArgumentException('Registration number is required and must be less or equal 16 characters');
         }
         if (empty($vehicleDTO->brand) || strlen($vehicleDTO->brand) > 60) {
-            throw new \InvalidArgumentException('Brand is required and must be less or equal than 60 characters');
+            throw new \InvalidArgumentException('Brand is required and must be less or equal 60 characters');
         }
         if (empty($vehicleDTO->model) || strlen($vehicleDTO->model) > 60) {
-            throw new \InvalidArgumentException('Model is required and must be less or equal than 60 characters');
+            throw new \InvalidArgumentException('Model is required and must be less or equal 60 characters');
         }
         if (empty($vehicleDTO->type)) {
             throw new \InvalidArgumentException('Type is required');
