bc6564853fc6e790263d88c1091b3156e0d9a4ec
Grzegorz Kadaj <grzegorz.kadaj@devmouse.pl>
2025-10-20 14:18:11 +0200

    Comment for CORS

--- app/Controller/CORSHeader.php
+++ app/Controller/CORSHeader.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 class CORSHeader
 {
+    /** TODO change CORS configuration in production */
     public static array $headers = [
         'Access-Control-Allow-Origin' => '*',
     ];
